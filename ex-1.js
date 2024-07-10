d1e2be1b2949fc7a8ee2cd13396edd401058a3b5
Panpak Apesariyayo <ingpanpak@gmail.com>
2024-07-10 14:43:56 +0700

    Algor

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,19 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  let result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = 1 + i; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        result.push(i, j);
+      }
+    }
+    if (result.length === 2) {
+      break;
+    }
+  }
+  return result;
+}
+
+let numbers1 = [2, 7, 11, 15];
+let target1 = 9;
+
+console.log(twoSum(numbers1, target1));
